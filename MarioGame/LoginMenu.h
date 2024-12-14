8ba8424866da3873490e6e19cb118727f1370884
Đại Vũ Đinh <dinhdaivu1109@gmail.com>
2024-12-14 19:42:26 +0700

    Update Register Button

--- MarioGame/LoginMenu.h
+++ MarioGame/LoginMenu.h
@@ -9,28 +9,38 @@ class LoginMenu
 private:
     //Font
     sf::Font font;
+
     // Background
     sf::Texture backgroundTexture;
     sf::Sprite backgroundSprite;
+
     //Login Buttons
     sf::Text usernameText;
     sf::Text passwordText;
     sf::Text loginText;
+    sf::Text registerText;
+
 
     // Input fields
     sf::Text usernameInput;
     sf::Text passwordInput;
+
     sf::RectangleShape usernameBox;
     sf::RectangleShape passwordBox;
     sf::RectangleShape loginButton;
+    sf::RectangleShape registerButton;
+
     // Hide password
     sf::Texture passwordTexture;
     sf::Sprite passwordSprite;
     sf::Text errorMessage;
+
     std::string username = "";
     std::string password = "";
+
     bool isUsernameSelected;
     bool isPasswordSelected;
+
 public:
     // Functions
     void Begin(sf::RenderWindow& window);
